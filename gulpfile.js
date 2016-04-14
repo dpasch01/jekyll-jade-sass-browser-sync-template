f8966729c8157fb09f23690198bdeb6b434da1d8
Demetris Paschalides <dpasch01@cs.ucy.ac.cy>
2016-04-14 08:50:00 +0300

    Jekyll project setup.

---
+++ gulpfile.js
@@ -0,0 +1,57 @@
+var gulp        = require('gulp');
+var browserSync = require('browser-sync');
+var sass        = require('gulp-sass');
+var prefix      = require('gulp-autoprefixer');
+var cp          = require('child_process');
+var jade        = require('gulp-jade');
+
+var jekyll   = process.platform === 'win32' ? 'jekyll.bat' : 'jekyll';
+var messages = {
+    jekyllBuild: '<span style="color: grey">Running:</span> $ jekyll build'
+};
+
+gulp.task('jekyll-build', function (done) {
+    browserSync.notify(messages.jekyllBuild);
+    return cp.spawn( jekyll , ['build'], {stdio: 'inherit'})
+        .on('close', done);
+});
+
+gulp.task('jekyll-rebuild', ['jekyll-build'], function () {
+    browserSync.reload();
+});
+
+gulp.task('browser-sync', ['sass', 'jekyll-build'], function() {
+    browserSync({
+        server: {
+            baseDir: '_site'
+        },
+        notify: false
+    });
+});
+
+gulp.task('sass', function () {
+    return gulp.src('assets/css/main.scss')
+        .pipe(sass({
+            includePaths: ['css'],
+            onError: browserSync.notify
+        }))
+        .pipe(prefix(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
+        .pipe(gulp.dest('_site/assets/css'))
+        .pipe(browserSync.reload({stream:true}))
+        .pipe(gulp.dest('assets/css'));
+});
+
+gulp.task('jade',function(){
+    return gulp.src('_jade/*.jade')
+        .pipe(jade())
+        .pipe(gulp.dest('_includes'));
+});
+
+gulp.task('watch', function () {
+    gulp.watch('assets/css/**', ['sass']);
+    gulp.watch(['*.html', '_layouts/*.html', '_posts/*', '_includes/*', '_carousel/*'], ['jekyll-rebuild']);
+    gulp.watch('_jade/*.jade', ['jade']);
+    gulp.watch('assets/js/**', ['jekyll-rebuild']);
+});
+
+gulp.task('default', ['browser-sync', 'watch']);
